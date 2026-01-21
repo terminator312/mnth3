cd8fe3ab3ac396fc211e86f39fccd5ebbc0e2db8
mixer <baihanjenishbekov9@gmail.com>
2026-01-21 16:35:43 +0600

    dz2

---
+++ dz2/script.js
@@ -0,0 +1,32 @@
+let number = 0;
+
+const value = document.querySelector("#value");
+const plusBtn = document.querySelector("#plus");
+const minusBtn = document.querySelector("#minus");
+
+plusBtn.onclick = () => {
+  number++;
+  value.textContent = number;
+  value.style.color = "green";
+};
+
+minusBtn.onclick = () => {
+  if (number > 0) {
+    number--;
+    value.textContent = number;
+    value.style.color = "red";
+  }
+};
+
+const box = document.querySelector("#box")
+const coords = document.querySelector("#coords")
+
+
+box.onmousemove = (event) => {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  coords.textContent = `X: ${x}, Y: ${y}`;
+};
+
+
+
