b2d915f81a5a16bbbcf8d8281def3ecd52b185db
terminator2.0 <m1xerotg@gmail.com>
2026-01-26 12:51:35 +0600

    dz3

--- dz3/script.js
+++ dz3/script.js
@@ -1,4 +1,38 @@
+const todoList = document.getElementById("todo-list");
+const addBtn = document.getElementById("add-btn");
+const todoInput = document.getElementById("todo-input");
 
+addBtn.addEventListener("click", () => {
+  const value = todoInput.value;
+  if (!value.trim()) return;
+
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+    <label>
+      <input type="checkbox" class="todo-checkbox" />
+      <span class="todo-title">${value}</span>
+    </label>
+    <button class="deleteBtn">Удалить</button>
+  `;
+
+  const deleteBtn = li.querySelector(".deleteBtn");
+  deleteBtn.addEventListener("click", () => {
+    li.remove();
+  });
+
+  const checkbox = li.querySelector(".todo-checkbox");
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      li.classList.add("completed");
+    } else {
+      li.classList.remove("completed");
+    }
+  });
+
+  todoList.appendChild(li);
+  todoInput.value = "";
+});
 
 
 
